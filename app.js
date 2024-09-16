2a2d346979df31d9f2062699001dca57ca57ee94
Carlos Nuno <lykos94@yahoo.com>
2024-09-15 21:37:41 -0500

    init commit

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 let name = prompt("Hello! What is your name?");
-alert("nice to meet you, " + name + "!");
+alert("Nice to meet you, " + name + "!");
 document.write("Hello " + name + ", welcome to my website!")
 
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.nav-menu');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+})
