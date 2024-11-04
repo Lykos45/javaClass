0ca3ce01ec789c9260efa6958fd88837551d8d55
Carlos Nuno <lykos94@yahoo.com>
2024-11-03 22:15:03 -0600

    init commit

---
+++ jquery-events/glossary.js
@@ -0,0 +1,68 @@
+/*Botanic names*/
+//Hiding botanic names & pictures when page loads
+$(document).ready(function() {
+  $(".botanic").hide();
+  $(".imgdiv").hide();
+});
+//Show botanic names when user clicks on flower names
+$(document).ready(function() {
+  $(".flower").click(function() {
+    $(this).children('.botanic').show();
+  })
+});
+
+/*mouseover and mouseout plant name to show pictures (hide/show) NOT FINISHED*/
+//creating image variables
+var getElementById = ("coneflower");
+var getElementById = ("mum");
+var getElementById = ("rose");
+//Using hover to show image to user
+$('.pic').hover(function() {
+
+  $('#coneflower').show();
+  
+  },function(){
+  //Using hide to conceal image to user
+  $('#coneflower').hide();
+  
+  });
+  //Using hover to show image to user
+  $('.pic').hover(function() {
+  
+  $('#mum').show();
+  
+  },function(){
+  //Using hide to conceal image to user
+  $('#mum').hide();
+  
+  });
+  //Using hover to show image to user
+  $('.pic').hover(function() {
+  
+  $('#rose').show();
+  
+  },function(){
+  //Using hide to conceal image to user
+  $('#rose').hide();
+  
+  });  
+/*CODE ABOVE IS NOT DONE COULD NOT FIGURE OUT^^^*/
+
+/*Keypress event*/
+$(document).ready(function() {
+  $(document).on('keypress', function(e) {
+    // Check if the pressed key is a letter
+    if (e.which >= 65 && e.which <= 90) { 
+      var letter = String.fromCharCode(e.which).toLowerCase();
+      // Finds the element with the matching ID when user presses shift key and a letter
+      var targetElement = $('#' + letter); 
+      // If the element exists, it will scroll to it
+      if (targetElement.length) {
+        $('html, body').animate({
+          scrollTop: targetElement.offset().top
+        }, 500);
+      }
+    }
+  });
+});
+//End program
